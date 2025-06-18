c34aa7ac3973cb317e4405cdc319a55e1be94eb4
Tamir Mishali <tamirmishali@gmail.com>
2025-06-18 11:54:52 +0300

    fixed DB loading and backedup my db
    Signed-off-by: Tamir Mishali <tamirmishali@gmail.com>

--- app/src/main/java/com/example/tamirmishali/trainingmanager/Database/TrainingManagerDatabase.java
+++ app/src/main/java/com/example/tamirmishali/trainingmanager/Database/TrainingManagerDatabase.java
@@ -59,8 +59,10 @@ public abstract class TrainingManagerDatabase extends RoomDatabase {
     public abstract ExerciseAbstractNicknameDao exerciseAbstractNicknameDao();
 
     // Prevention of opening the same database to RAM twice.
-//    private static final String DATABASE_NAME = "training_manager_database_23-07-2023.db";
-    private static final String DATABASE_NAME = "training_manager_database_07-11-2024.db";
+//    private static final String DATABASE_NAME = "training_manager_database_23-07-2023.db"; // Thats the one that worked for Shani
+    private static final String DATABASE_NAME = "training_manager_database.db";
+//    private static final String DATABASE_NAME = "training_manager_database_07-11-2024.db";
+//    private static final String DATABASE_NAME = "training_manager_database-clean.db";
     private static volatile TrainingManagerDatabase INSTANCE;
 
     public static synchronized TrainingManagerDatabase getInstance(Context context) {
